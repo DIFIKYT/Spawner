6ac1eaf89fb020dea3607a0a74f1fc77fb6d4027
DIFIKYT <ragexses@mail.ru>
2024-02-03 02:08:41 +0700

    вторая версия

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,12 +3,6 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
-    [SerializeField] private float _destroyDelay = 5f;
-
-    private void Start()
-    {
-        Invoke(nameof(DestroyEnemy), _destroyDelay);
-    }
 
     private void Update()
     {
@@ -18,10 +12,5 @@ public class Enemy : MonoBehaviour
     private void Move()
     {
         transform.Translate(Vector3.forward * _speed * Time.deltaTime);
-    }
-
-    private void DestroyEnemy()
-    {
-        Destroy(gameObject);
     }
 }
